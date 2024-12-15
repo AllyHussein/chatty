366bdfb310eb93d37f7d65758b5c026628d4a15f
AllyHussein <alihussein95@outlook.com>
2024-12-15 22:30:13 +0200

    revert

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:5001/api"
-      : "https://chatty-ecru-nine.vercel.app/api",
+      : "/api",
   withCredentials: true,
 });
